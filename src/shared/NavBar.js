ee10863632bdf6a28f7d0c7ebf751bb801fe3cb5
Carolina Uribe <curib017@fiu.edu>
2020-02-10 23:31:39 -0500

    modify navbar with needed components + change favicon

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography'
 
 import logo from '../assets/AdvanceLogo.png';
 
 const NavBar = () => {
     const classes = useStyles();
     return (
-        <div>
-            <AppBar position="static">
+        <div className={classes.root}>
+            <AppBar position="static" style={{ background: '#7C629D' }}>
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <IconButton edge="start" color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton>
                     <img src={logo} alt='logo' className={classes.logo} />
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit" className={classes.loginButton}>Login</Button>
                 </Toolbar>
             </AppBar>
         </div>
@@ -30,17 +30,14 @@ const useStyles = makeStyles(
     createStyles({
         logo: {
             maxWidth: 160,
+            marginLeft: 10,
         },
         root: {
             flexGrow: 1,
-          },
-        //   menuButton: {
-        //     marginRight: theme.spacing(2),
-        //   },
-          title: {
-            flexGrow: 1,
-          },
-
+        },
+        loginButton: {
+            marginLeft: "auto"
+        }
     })
 );
 

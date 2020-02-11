0e9a3905b75a19527d57d13b9bf9ed691271e10c
Carolina Uribe <curib017@fiu.edu>
2020-02-10 22:06:24 -0500

    import needed material ui icons needed for the navbar

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -3,28 +3,45 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import logo from '../assets/AdvanceLogo.png';
 
 const NavBar = () => {
     const classes = useStyles();
-    return(
+    return (
         <div>
-        <AppBar position="static">
-            <Toolbar>
-                <img src={logo} alt='logo' className={classes.logo} />
-            </Toolbar>
-        </AppBar>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <img src={logo} alt='logo' className={classes.logo} />
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
 
 const useStyles = makeStyles(
     createStyles({
-    logo: {
-        maxWidth: 160,
-    },
-  })
+        logo: {
+            maxWidth: 160,
+        },
+        root: {
+            flexGrow: 1,
+          },
+        //   menuButton: {
+        //     marginRight: theme.spacing(2),
+        //   },
+          title: {
+            flexGrow: 1,
+          },
+
+    })
 );
 
 

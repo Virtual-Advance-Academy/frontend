5a3a54d9f3638a638fa5995999de4aa1bcb97951
Carolina Uribe <curib017@fiu.edu>
2020-02-10 19:41:40 -0500

    place logo in navbar

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -2,18 +2,30 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+import logo from '../assets/AdvanceLogo.png';
+
 const NavBar = () => {
+    const classes = useStyles();
     return(
         <div>
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="title" color="inherit">
-                Advance Academy 
-                {/* <img src = './assets/AdvanceLogo.png'/> */}
-                </Typography>
+                <img src={logo} alt='logo' className={classes.logo} />
             </Toolbar>
         </AppBar>
         </div>
     )
 }
+
+const useStyles = makeStyles(
+    createStyles({
+    logo: {
+        maxWidth: 160,
+    },
+  })
+);
+
+
 export default NavBar;

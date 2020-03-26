45d204a1e6824db520dfab0a19623eb8957d5aa4
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-03-26 10:04:16 -0400

    Made UserAvatar into its own component

---
+++ src/shared/UserAvatar.js
@@ -0,0 +1,37 @@
+import React, { useGlobal } from "reactn";
+import clsx from "clsx";
+import { makeStyles, Avatar, Typography } from "@material-ui/core";
+
+const UserAvatar = props => {
+    const classes = useStyles();
+
+    const [user] = useGlobal("user");
+    const firstLetter = user.name
+        .toUpperCase()
+        .split(" ")[0]
+        .split("")[0];
+    const lastLetter = user.name
+        .toUpperCase()
+        .split(" ")[1]
+        .split("")[0];
+    return (
+        <Avatar
+            {...props}
+            className={clsx(classes.UserAvatar, props.className)}
+        >
+            <Typography variant="inherit">
+                {firstLetter}
+                {lastLetter}
+            </Typography>
+        </Avatar>
+    );
+};
+
+const useStyles = makeStyles(theme => ({
+    UserAvatar: {
+        boxSizing: "content-box",
+        background: theme.palette.primary.main
+    }
+}));
+
+export default UserAvatar;

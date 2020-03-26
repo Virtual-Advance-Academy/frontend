9ba7e06f01f96160654cbc7a791fa9beff035cb0
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-03-26 09:26:42 -0400

    Added profile page

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,25 +1,108 @@
-import React from "react";
+import React, { useState, useGlobal } from "reactn";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Grid } from "@material-ui/core";
+import { Typography, Grid, Paper, TextField } from "@material-ui/core";
 import AuthorizedRoute from "shared/AuthorizedRoute";
+import { useClient } from "utils/Client";
+import { useEffect } from "react";
 
 const Profile = () => {
     const classes = styles();
 
+    const [jwt] = useGlobal("jwt");
+
+    const Client = useClient(jwt);
+
+    const [profile, setProfile] = useState({});
+
+    const { fullName = "", email = "", username = "" } = profile;
+
+    useEffect(() => {
+        const fetch = async () => {
+            let res = await Client.getProfile();
+            setProfile(res.data);
+        };
+        fetch();
+    }, []);
+
     return (
         <>
             <AuthorizedRoute />
-            <Typography>Test</Typography>
+            <Grid
+                container
+                className={classes.page}
+                justify="center"
+                alignContent="center"
+            >
+                <Grid item container lg={4}>
+                    <Grid item className={classes.profileTitle}>
+                        <Typography
+                            variant="h3"
+                            component="h1"
+                            align="center"
+                            color="primary"
+                        >
+                            Account Information
+                        </Typography>
+                    </Grid>
+
+                    <Grid
+                        item
+                        container
+                        spacing={1}
+                        component={Paper}
+                        variant="outlined"
+                        className={classes.profileBox}
+                    >
+                        <Grid item container xs={12}>
+                            <Grid item>
+                                <Typography variant="button">Photo</Typography>
+                                <Typography variant="body1" paragraph>
+                                    A photo helps personalize your account.
+                                </Typography>
+                            </Grid>
+                            <Grid item></Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="button">Name</Typography>
+                            <Typography variant="body1" paragraph>
+                                {fullName}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="button">Username</Typography>
+                            <Typography variant="body1" paragraph>
+                                {username}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="button">Email</Typography>
+                            <Typography variant="body1" paragraph>
+                                {email}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="button">Password</Typography>
+                            <Typography variant="body1" paragraph>
+                                ********
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </>
     );
 };
 
 const styles = makeStyles(theme => ({
-    mainTitle: {
-        display: "inline-block",
-        "& span": {
-            display: "block"
+    profileTitle: {
+        width: "100%",
+        padding: `${theme.spacing(4)}px 0`,
+        "& h1": {
+            fontWeight: theme.typography.fontWeightLight
         }
+    },
+    profileBox: {
+        padding: `${theme.spacing(4)}px ${theme.spacing(6)}px `
     },
     titleSpacing: {
         marginRight: "80px",

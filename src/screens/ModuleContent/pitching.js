31ffc9cefb7eddc6ff40bf63101f3ed76a1d0f7f
Carolina Uribe-Gosselin <curib017@fiu.edu>
2020-04-11 14:31:12 -0400

    format beginning of module with styling and introduction

--- src/screens/ModuleContent/pitching.js
+++ src/screens/ModuleContent/pitching.js
@@ -3,6 +3,7 @@ import { Grid, Typography, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import ModuleContent from "../../shared/modules/ModuleContent";
 import modules from "../../shared/modules/modules";
+import ModulePagination from "shared/modules/ModulePagination";
 
 const Pitching = () => {
     const classes = styles();
@@ -10,14 +11,98 @@ const Pitching = () => {
         (module) => module.title === "Pitching Yourself"
     );
     return (
-        <ModuleContent
-            title={pitching.title}
-            description={pitching.description}
-            image={pitching.image}
-        />
+        <Container className={classes.container}>
+            <ModuleContent
+                title={pitching.title}
+                description={pitching.description}
+                image={pitching.image}
+            />
+            <ModulePagination>
+                <ModulePagination.Page>
+                    <Grid container>
+                        <Typography
+                            className={classes.contentHeader}
+                            variant="h5"
+                            color="primary"
+                            gutterBottom
+                        >
+                            Introduction
+                        </Typography>
+                    </Grid>
+                    <Grid container justify="center" spacing={2}>
+                        <Grid item>
+                            <Typography
+                                className={classes.contentDescription}
+                                variant="body1"
+                            >
+                                Developing a concise, personal, and thoughtful
+                                pitch will get you noticed in person or online.
+                                Beyond skills, recruiters are looking for
+                                someone they would enjoy working with. When
+                                pitching, not only do you want to mention the
+                                skills relevant to the position, but that you
+                                can get along well with the other people who
+                                will be working with you.
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </ModulePagination.Page>
+            </ModulePagination>
+        </Container>
     );
 };
 
-const styles = makeStyles((theme) => ({}));
+const styles = makeStyles((theme) => ({
+    container: {
+        maxWidth: 1500
+    },
+    contentHeader: {
+        fontWeight: "700",
+        padding: theme.spacing(3),
+        marginLeft: "10%"
+    },
+    subContentHeader: {
+        fontWeight: "500",
+        padding: theme.spacing(3),
+        textAlign: "center",
+        justify: "center"
+    },
+    contentDescription: {
+        textAlign: "left",
+        justify: "center",
+        color: "#D8D8D8",
+        maxWidth: 800,
+        display: "block",
+        "& span": {
+            display: "block"
+        }
+    },
+    subDescription: {
+        textAlign: "left",
+        justify: "center",
+        color: "#D8D8D8",
+        maxWidth: 600,
+        display: "block",
+        "& span": {
+            display: "block"
+        }
+    },
+    titleSpacing: {
+        textAlign: "left",
+        marginRight: "80px",
+        [theme.breakpoints.down("xs")]: {
+            marginRight: "unset"
+        }
+    },
+    root: {
+        maxWidth: 1000
+    },
+    media: {
+        paddingTop: "68.9655172%"
+    },
+    downloadHere: {
+        marginLeft: "auto"
+    }
+}));
 
 export default Pitching;

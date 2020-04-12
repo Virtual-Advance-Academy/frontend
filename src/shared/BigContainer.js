eb6126df141e47f5e5930169817170b2ff1ac140
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-04-12 13:17:37 -0400

    Fixed login and home page

---
+++ src/shared/BigContainer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { makeStyles, Container } from "@material-ui/core";
+
+const BigContainer = ({ children }) => {
+    const classes = styles();
+    return <Container className={classes.container}>{children}</Container>;
+};
+
+const styles = makeStyles((theme) => ({
+    container: {
+        maxWidth: 1500,
+        minHeight: "100vh"
+    }
+}));
+
+const withBigContainer = (ComponentToWrap) => {
+    return (
+        <BigContainer>
+            <ComponentToWrap />
+        </BigContainer>
+    );
+};
+
+export default BigContainer;
+export { withBigContainer };

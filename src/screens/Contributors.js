4c10a52534b2a231bf0cc302c3260059ec5df8ee
Carolina Uribe-Gosselin <curib017@fiu.edu>
2020-04-13 22:33:47 -0400

    create a new card component for the contributors and add in data for contributors

--- src/screens/Contributors.js
+++ src/screens/Contributors.js
@@ -2,8 +2,22 @@ import React, { useGlobal, useState, useEffect } from "reactn";
 import { Grid, Typography, Zoom } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import ModuleCard from "../shared/modules/ModuleCard";
-import modules from "shared/modules/moduleData";
+import contributors from "../shared/ContributorsData";
 import ScrollToTop from "shared/ScrollToTop";
+import {
+    Avatar,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    CircularProgress
+} from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
+import { CheckCircle } from "@material-ui/icons";
+import clsx from "clsx";
+import { Link, useRouteMatch } from "react-router-dom";
 
 const Contributors = () => {
     const classes = styles();
@@ -21,8 +35,14 @@ const Contributors = () => {
                     Contributors
                 </Typography>
             </Grid>
-            <Grid item container spacing={0} className={classes.moduleGrid}>
-                {modules.map((module, i) => {
+            <Grid
+                item
+                container
+                spacing={0}
+                justify="center"
+                className={classes.moduleGrid}
+            >
+                {contributors.map((contributor, i) => {
                     return (
                         <Zoom
                             in
@@ -30,13 +50,16 @@ const Contributors = () => {
                                 transitionDelay: `calc(.25s + ${i * 100}ms)`
                             }}
                         >
-                            <Grid key={module.id} item className={classes.card}>
-                                <ModuleCard
-                                    image={module.image}
-                                    title={module.title}
-                                    description={module.description}
-                                    slug={module.slug}
-                                    icon={module.icon}
+                            <Grid
+                                key={contributor.id}
+                                item
+                                className={classes.card}
+                            >
+                                <ContributorCard
+                                    image={contributor.image}
+                                    name={contributor.name}
+                                    biography={contributor.biography}
+                                    grad={contributor.grad}
                                 />
                             </Grid>
                         </Zoom>
@@ -44,6 +67,64 @@ const Contributors = () => {
                 })}
             </Grid>
         </>
+    );
+};
+
+const ContributorCard = ({ image, name, biography, grad, loading = false }) => {
+    let [raised, setRaised] = useState(false);
+    const classes = styles();
+    const match = useRouteMatch();
+
+    let imgSrc;
+
+    try {
+        imgSrc = require(`assets/contributors/${image}.jpg`);
+    } catch (e) {
+        imgSrc = "";
+    }
+
+    return (
+        <Card
+            raised={raised}
+            onMouseEnter={() => setRaised(true)}
+            onMouseLeave={() => setRaised(false)}
+            className={clsx(classes.moduleCard)}
+            // Light up the card on hover
+            style={raised ? { opacity: 1 } : {}}
+            classes={{
+                root: classes.cardRoot
+            }}
+        >
+            <div>
+                <CardHeader
+                    avatar={
+                        <Avatar
+                            alt="Contributor"
+                            src={imgSrc}
+                            className={classes.contributorImg}
+                        />
+                    }
+                    classes={{
+                        action: classes.actionOverride
+                    }}
+                />
+                <CardContent>
+                    {!loading && (
+                        <Typography variant="body2">{biography}</Typography>
+                    )}
+                    {loading && (
+                        <>
+                            <Skeleton variant="text" />
+                            <Skeleton variant="text" />
+                            <Skeleton variant="text" />
+                            <Skeleton variant="text" />
+                            <Skeleton variant="text" />
+                            <Skeleton variant="text" width="85%" />
+                        </>
+                    )}
+                </CardContent>
+            </div>
+        </Card>
     );
 };
 
@@ -58,6 +139,47 @@ const styles = makeStyles((theme) => ({
     moduleGrid: {
         display: "grid",
         gridTemplateColumns: "repeat(auto-fill, minmax(290px, 1fr))"
+    },
+    moduleCard: {
+        // Provide some spacing between cards
+        margin: 8,
+
+        // Use flex layout with column direction for components in the card
+        // (CardContent and CardActions)
+        display: "flex",
+        flexDirection: "column",
+
+        // Justify the content so that CardContent will always be at the top of the card,
+        // and CardActions will be at the bottom
+        justifyContent: "space-between",
+        flexGrow: 1
+    },
+    cardRoot: {
+        backgroundColor: "#2b2730"
+    },
+    avatar: {
+        backgroundColor: "transparent",
+        color: theme.palette.common.white,
+        transform: "rotateY(180deg)",
+        position: "absolute",
+        backfaceVisibility: "hidden"
+    },
+    actionOverride: {
+        marginTop: 0
+    },
+    contributorImg: {
+        width: theme.spacing(20),
+        height: theme.spacing(20)
+        // alignSelf: "center",
+        // display: "flex",
+        // alignItems: "center",
+        // justifyContent: "center",
+        // flex: 1,
+        // display: "flex"
+    },
+    heading: {
+        alignItems: "center",
+        justifyContent: "center"
     }
 }));
 
---
+++ src/shared/ContributorsData.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        id: "contributor-1",
+        name: "Christopher Rodriguez",
+        grad: "Spring 2020",
+        biography:
+            "Learn about various types of internships available in the field, where to find internships, the different internship seasons & how to decide which companies are best for you based on your goals and interests.",
+        image: "ChrisR"
+    },
+    {
+        id: "contributor-2",
+        name: "Carolina Uribe-Gosselin",
+        grad: "Spring 2020",
+        biography:
+            "Carolina studied Computer Science and Psychology at FIU. During her undergrad degree, she co-founded a software development program, SparkDev, she was the Worshops Coordinator for ShellHacks for 2 years, and was an active member in the community. She is now a Program Manager at Microsoft in the Visual Studio & .NET Team.",
+        image: "CarolinaU"
+    }
+];

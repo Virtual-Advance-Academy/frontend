59d5ec376ffeba52e9e34d036723f7d3e9b09b63
Carolina Uribe-Gosselin <curib017@fiu.edu>
2020-04-26 17:31:28 -0400

    add in the skelton for what to wear and types of internship + annoucement

--- src/screens/ModuleContent/types-of-internships.js
+++ src/screens/ModuleContent/types-of-internships.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ModuleContent from "../../shared/modules/ModuleContent";
 import modules from "../../shared/modules/moduleData";
+import ModulePagination from "shared/modules/ModulePagination";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
 
 const InternshipTypes = () => {
     const [internships] = modules.filter(
@@ -13,8 +15,29 @@ const InternshipTypes = () => {
             title={internships.title}
             description={internships.description}
             image={internships.image}
-        />
+        >
+            <ModulePagination>
+                <ModulePagination.Page>
+                    <Grid container justify="center">
+                        <Typography
+                            className={classes.annoucement}
+                            variant="h2"
+                            color="primary"
+                            gutterBottom
+                        >
+                            Coming Soon...
+                        </Typography>
+                    </Grid>
+                </ModulePagination.Page>
+            </ModulePagination>
+        </ModuleContent>
     );
 };
 
+const styles = makeStyles((theme) => ({
+    annoucement: {
+        fontWeight: "700",
+        padding: "10%"
+    }
+}));
 export default InternshipTypes;
